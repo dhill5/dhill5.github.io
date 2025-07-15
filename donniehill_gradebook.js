4469c6cad2f8d7db13c65674996cced3457ed464
dhill5 <dhill5@my.waketech.edu>
2025-07-14 23:57:25 -0400

    created gradebook

---
+++ donniehill_gradebook.js
@@ -0,0 +1,11 @@
+function fetchGradeData() {
+    console.log("fetchGradeData function called.");
+}
+
+function populateGradebook() {
+    console.log("populateGradebook function called.");
+}
+
+// Temporary call to test in console
+fetchGradeData();
+populateGradebook();
