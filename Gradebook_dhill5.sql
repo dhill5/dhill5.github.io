0ce2cb74f67ded10cac19a7a30d139d579d38aa4
dhill5 <dhill5@my.waketech.edu>
2025-07-14 23:44:10 -0400

    Updates

---
+++ Gradebook_dhill5.sql
@@ -0,0 +1,197 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.5
+-- Dumped by pg_dump version 17.5
+
+-- Started on 2025-06-16 20:22:12 EDT
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 220 (class 1259 OID 16419)
+-- Name: assignments; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.assignments (
+    assignment_id integer NOT NULL,
+    title character varying(100),
+    description text,
+    due_date date,
+    student_id integer
+);
+
+
+ALTER TABLE public.assignments OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 16418)
+-- Name: assignments_assignment_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.assignments_assignment_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.assignments_assignment_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3616 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: assignments_assignment_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.assignments_assignment_id_seq OWNED BY public.assignments.assignment_id;
+
+
+--
+-- TOC entry 218 (class 1259 OID 16412)
+-- Name: students; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.students (
+    student_id integer NOT NULL,
+    first_name character varying(50),
+    last_name character varying(50),
+    email character varying(100),
+    enrollment_date date
+);
+
+
+ALTER TABLE public.students OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 16411)
+-- Name: students_student_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.students_student_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.students_student_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3617 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: students_student_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.students_student_id_seq OWNED BY public.students.student_id;
+
+
+--
+-- TOC entry 3456 (class 2604 OID 16422)
+-- Name: assignments assignment_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.assignments ALTER COLUMN assignment_id SET DEFAULT nextval('public.assignments_assignment_id_seq'::regclass);
+
+
+--
+-- TOC entry 3455 (class 2604 OID 16415)
+-- Name: students student_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.students ALTER COLUMN student_id SET DEFAULT nextval('public.students_student_id_seq'::regclass);
+
+
+--
+-- TOC entry 3610 (class 0 OID 16419)
+-- Dependencies: 220
+-- Data for Name: assignments; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.assignments (assignment_id, title, description, due_date, student_id) FROM stdin;
+1	Lab 1: PostgreSQL	Create tables and insert data into the Gradebook database.	2025-01-16	1
+2	Lab 7: PostgreSQL	Create tables and insert data into the Gradebook database as instructed.	2025-01-16	1
+\.
+
+
+--
+-- TOC entry 3608 (class 0 OID 16412)
+-- Dependencies: 218
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.students (student_id, first_name, last_name, email, enrollment_date) FROM stdin;
+1	Donnie	Hill	dhill5@my.waketech.edu	2025-01-16
+\.
+
+
+--
+-- TOC entry 3618 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: assignments_assignment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.assignments_assignment_id_seq', 2, true);
+
+
+--
+-- TOC entry 3619 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: students_student_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.students_student_id_seq', 1, true);
+
+
+--
+-- TOC entry 3460 (class 2606 OID 16426)
+-- Name: assignments assignments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.assignments
+    ADD CONSTRAINT assignments_pkey PRIMARY KEY (assignment_id);
+
+
+--
+-- TOC entry 3458 (class 2606 OID 16417)
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (student_id);
+
+
+--
+-- TOC entry 3461 (class 2606 OID 16427)
+-- Name: assignments assignments_student_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.assignments
+    ADD CONSTRAINT assignments_student_id_fkey FOREIGN KEY (student_id) REFERENCES public.students(student_id);
+
+
+-- Completed on 2025-06-16 20:22:12 EDT
+
+--
+-- PostgreSQL database dump complete
+--
+
